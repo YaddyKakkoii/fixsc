2754e69b96164b5c7bb84bc092865f097523df9b
YaddyKakkoii <127028273+YaddyKakkoii@users.noreply.github.com>
2023-05-22 16:09:42 +0700

    Update updatesc.sh

--- updatesc.sh
+++ updatesc.sh
@@ -4,5 +4,8 @@ echo -e "\e[32mUpdate Menu cepat sudah tersedia!!\e[0m";
 sleep 1
 echo -e "\e[mulai mengupdate script!!\e[0m";
 bash -c "$(wget -qO- https://raw.githubusercontent.com/YaddyKakkoii/fixsc/main/update.sh)"
+#wget -q -O update.sh https://raw.githubusercontent.com/YaddyKakkoii/fixsc/main/update.sh && chmod +x update.sh && ./update.sh
 rm -f update.sh
 echo "done....."
+echo -e "\e[32mUpdate Selesai...\e[0m"
+cd
