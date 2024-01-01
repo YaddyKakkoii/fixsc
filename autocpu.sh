fcb1391c2210f0322d7d1eab7b3e027c1720eb37
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-01-01 08:51:41 +0700

    Update autocpu.sh

--- autocpu.sh
+++ autocpu.sh
@@ -1,4 +1,110 @@
 #!/bin/bash
+function fixhaproxydanxray(){
+#!/bin/bash
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+yellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+grenbo="\e[92;1m"
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+KEY=6654578153:AAEL08DFbdrgtirDqtBU6DkP_acq31uZPik
+CHATID=1117211252
+#CHATID=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 3)
+#KEY=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 2)
+export TIME="10"
+export URL="https://api.telegram.org/bot$KEY/sendMessage"
+clear
+function notif_ssl() {
+green "Notif AddHost Tele"
+sleep 2
+#CHATID="$CHATID"    #KEY="$KEY"    #TIME="$TIME"    #URL="$URL"
+CHATID="1117211252"
+KEY="6654578153:AAEL08DFbdrgtirDqtBU6DkP_acq31uZPik"
+TIME="10"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+domain=$(cat /etc/xray/domain)
+TEXT="
+<code>====================</code>
+<code>Notif Pasang SSL    </code>
+<b>DOMAIN</b> <code>$domain</code>
+<b>FIXED XRAY OFFLINE DONE</b>
+<b>FIXED HAPROXY OFFLINE DONE</b>
+<code>====================</code>
+<code>PASANG SSL DONE</code>
+<code>====================</code>
+"
+
+curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+}
+
+function pasang_ssl() {
+bang="\033[1;93m"
+purple="\033[1;95m"
+NC='\033[0m'
+    green "Renew Cert SSL"
+    sleep 2
+    rm -rf /etc/xray/xray.key
+    rm -rf /etc/xray/xray.crt
+    domain=$(cat /etc/xray/domain)
+    STOPWEBSERVER=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+    rm -rf /root/.acme.sh
+    mkdir /root/.acme.sh
+    systemctl stop $STOPWEBSERVER
+    systemctl stop nginx
+    systemctl stop haproxy
+    curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+    chmod +x /root/.acme.sh/acme.sh
+    /root/.acme.sh/acme.sh --upgrade --auto-upgrade
+    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+    /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    chmod 777 /etc/xray/xray.key
+    clear
+    echo -e  "${bang}╭════════════════════════════════════════╮${NC}"
+    echo -e "${bang}│  $NC   ${purple}    FIXED HAPROXY OFFLINE   $NC    ${bang}   │${NC}"
+    echo -e  "${bang}╰════════════════════════════════════════╯${NC}"
+    echo -e ""
+    echo -e "  \033[1;91m service fixed haproxy\033[1;37m"
+    rm -fr /etc/haproxy/hap.pem
+    cat /etc/xray/xray.crt /etc/xray/xray.key | tee /etc/haproxy/hap.pem
+    echo -e "  \033[1;91m service restart daemon-reload\033[1;37m"
+    systemctl daemon-reload
+    systemctl restart nginx
+    systemctl restart xray
+    echo -e "  \033[1;91m service restart haproxy\033[1;37m"    
+    systemctl restart haproxy
+}
+netfilter-persistent restart
+clear
+echo -e  "${bang}╭════════════════════════════════════════╮${NC}"
+echo -e "${bang}│  $NC   ${purple}    FIXED XRAY OFFLINE   $NC       ${bang}   │${NC}"
+echo -e  "${bang}╰════════════════════════════════════════╯${NC}"
+echo -e ""
+echo -e "  \033[1;91m service fixed xray\033[1;37m"
+#wget -O /etc/systemd/system/runn.service "https://wokszxdstore.net/xray/runn.service" >/dev/null 2>&1
+echo -e "  \033[1;91m service restart xray\033[1;37m"
+systemctl restart xray
+echo -e""
+pasang_ssl
+notif_ssl
+echo ""
+}
 cd /etc
 nmap -p 22 $(curl -sS ipv4.icanhazip.com) > cekip
 cpu=$(grep -c -E "open" "cekip")
@@ -13,6 +119,7 @@ xray2=$(systemctl status xray | grep Active | awk '{print $3}' | cut -d "(" -f2 
     else
         systemctl restart xray
         systemctl start xray
+        fixhaproxydanxray
     fi
 nginx2=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
     if [[ $nginx2 == "running" ]]; then
