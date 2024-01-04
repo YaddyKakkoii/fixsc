8b0e1f2e7ea9df84ffea5ca33fa2ed93b211a1bd
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-01-05 02:10:43 +0700

    Update fixcpu.sh

--- fixcpu.sh
+++ fixcpu.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
+apt-get install psmisc
+apt install nmap -y
 function cekcronjob(){
 cat> /etc/cron.d/autocpu << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 */7 * * * * root /usr/bin/autocpu
 END
+
 }
 
 function cekotherfile(){
 REPO="https://raw.githubusercontent.com/YaddyKakkoii/fixsc/main/"
+if [ ! -e /etc/cekip ]; then
+    cd /etc
+    nmap -p 22 $(curl -sS ipv4.icanhazip.com) > cekip
+    cd /root
+fi
     if [ ! -e /usr/bin/autocpu ]; then
         wget -qO /usr/bin/autocpu "${REPO}autocpu.sh" && chmod +x /usr/bin/autocpu
     else
         rm -f /usr/bin/autocpu && wget -qO /usr/bin/autocpu "${REPO}autocpu.sh" && chmod +x /usr/bin/autocpu
     fi
-    if [ ! -e /etc/cron.d/autocpu ]; then
-        cekcronjob
-    fi
+if [ ! -e /etc/cron.d/autocpu ]; then
+    cekcronjob
+fi
+
 }
 
 cekotherfile
 cekcronjob
+
 echo "Auto-Reboot CPU 100% TURN ON."
+
 sleep 1
 service cron restart
 sleep 2
